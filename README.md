fc24e9c2bbebb31e50b4fd34b948f858f331dd3d
Francesco Michelini <fmichelini@pomodoro.com>
2017-01-17 15:54:06 +0100

    Parameters on each element instead of global

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # white-in-view
+A simple script to check whether an element is in the viewport.
+
+# Installation
+Include the `white-in-view.min.js` script before the closing `<body>` tag.
+```
+<script src="PATH/TO/SCRIPT/white-in-view.min.js"></script>
+```
+Assign the `.white-in-view-check` class to each element you want to apply the script to.
+
+After that, call the `whiteInView()` function. That's it.
+
+Once an element enters the defined viewport the script applies the `.white-is-in-view` CSS class, then it's up to you to make the stuff do some magic.
+
+Check the section below for a list of the parameters to use.
+
+# Parameters
+
+Here's an example of an element with all the parameters applied on it.
+```
+<div class="my-element white-in-view-check" data-offset-top="150" data-offset-bottom="250" data-percentage="true"></div>
+```
+
+## data-offset-top
+The amount (in PX or %) of the vertical offset from the top.
+
+## data-offset-bottom
+The amount (in PX or %) of the vertical offset from the bottom.
+
+## data-percentage
+Whether the offset values are in percentage (related to the window height) instead of px.
+
+# Compatibility
+The script has been tested on:
+
+- Chrome 55
+- Firefox 50
+- Safari 9.1
+- Opera 42
+- Opera Neon 1
--- white-in-view.js
+++ white-in-view.js
@@ -1,4 +1,4 @@
-function whiteInView(offsetTop, offsetBottom, isPercentage) {
+function whiteInView() {
 
     'use strict';
 
@@ -6,21 +6,25 @@ function whiteInView(offsetTop, offsetBottom, isPercentage) {
         inViewElemsLength = inViewElems.length, 
         bounds            = null, 
         isInView          = false, 
-        offsetTop         = offsetTop || false, 
-        offsetBottom      = offsetBottom || false, 
-        isPercentage      = isPercentage || false;
+        offsetTop         = null, 
+        offsetBottom      = null, 
+        isPercentage      = null ,
+        current           = null;
 
     function checkBounds() {
         for(var i = 0; i < inViewElemsLength; i++) {
 
-            bounds = inViewElems[i].getBoundingClientRect();
+            bounds              = inViewElems[i].getBoundingClientRect();
+            offsetTop           = inViewElems[i].getAttribute('data-offset-top') ? parseInt(inViewElems[i].getAttribute('data-offset-top')) : 0;
+            offsetBottom        = inViewElems[i].getAttribute('data-offset-bottom') ? parseInt(inViewElems[i].getAttribute('data-offset-bottom')) : window.innerHeight;
+            isPercentage        = inViewElems[i].getAttribute('data-percentage') && inViewElems[i].getAttribute('data-percentage') === 'true';
 
             if ( isPercentage ) {
-                if ( offsetBottom ) bounds.offsetBottom = offsetBottom * window.innerHeight / 100;
-                if ( offsetTop ) bounds.offsetTop = offsetTop * window.innerHeight / 100;
+                if ( offsetBottom ) offsetBottom = offsetBottom * window.innerHeight / 100;
+                if ( offsetTop ) offsetTop = offsetTop * window.innerHeight / 100;
             }
 
-            isInView = bounds.top < (window.innerHeight - bounds.offsetBottom) && bounds.bottom > (0 + bounds.offsetTop);
+            isInView = bounds.top < (window.innerHeight - offsetBottom) && bounds.bottom > (0 + offsetTop);
 
             if ( isInView ) {
                 inViewElems[i].classList.add('white-is-in-view');
